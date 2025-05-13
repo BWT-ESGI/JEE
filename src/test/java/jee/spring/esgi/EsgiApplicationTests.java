b5326f5509cbbf39c85b5907965135cc27e8c1a0
benjamin <bollier0812@gmaill.com>
2025-05-13 20:54:22 +0200

    first setup project spring boot

---
+++ src/test/java/jee/spring/esgi/EsgiApplicationTests.java
@@ -0,0 +1,13 @@
+package jee.spring.esgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
